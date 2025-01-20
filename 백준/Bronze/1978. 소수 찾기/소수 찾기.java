c62feca9bf616dd08de59d9c388cc18e8280f95b
Songtaekho <thdxor17@gmail.com>
2025-01-20 17:33:46 +0900

    [Bronze II] Title: 소수 찾기, Time: 180 ms, Memory: 17792 KB -BaekjoonHub

---
+++ 백준/Bronze/1978. 소수 찾기/소수 찾기.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		Scanner scan = new Scanner(System.in);
+		int N = scan.nextInt();
+		int [] arr = new int[N];
+		int count = 0;
+		
+		for(int i = 0; i < arr.length; i++) {
+			int cnt = 0;
+			arr[i] = scan.nextInt();
+			for(int j = 1; j <= arr[i]; j++) {
+				if(arr[i] % j == 0) {
+					cnt++;	
+				}
+			}
+			if(cnt == 2) {
+				count++ ;
+			}
+		}
+		System.out.println(count);
+	}
+}
