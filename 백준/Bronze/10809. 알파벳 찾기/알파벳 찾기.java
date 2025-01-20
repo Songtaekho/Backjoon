a95fef2dec9f93c6206a6568c4044cf1695a8426
Songtaekho <thdxor17@gmail.com>
2025-01-20 17:32:29 +0900

    [Bronze II] Title: 알파벳 찾기, Time: 188 ms, Memory: 18292 KB -BaekjoonHub

---
+++ 백준/Bronze/10809. 알파벳 찾기/알파벳 찾기.java
@@ -0,0 +1,24 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		int a = 0;
+		Scanner scan = new Scanner(System.in);
+		
+		String S = scan.next();
+		
+		char [] arr2 = new char[26];
+		for(char i = 'a'; i <= 'z'; i++) {
+			arr2[a] += i;
+			a++;
+		}
+		
+		for(int j = 0; j < arr2.length; j++) {
+			int a2 = S.indexOf(arr2[j]);
+			System.out.print(a2 + " ");	
+		}
+
+	}
+}
