8dcd875aaad4f13ecf97d5917f1d2dc1dda59211
Songtaekho <thdxor17@gmail.com>
2025-02-04 13:53:12 +0900

    [Bronze III] Title: 약수 구하기, Time: 176 ms, Memory: 17748 KB -BaekjoonHub

---
+++ 백준/Bronze/2501. 약수 구하기/약수 구하기.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+
+		Scanner scan = new Scanner(System.in);
+
+		int N = scan.nextInt();
+		int K = scan.nextInt();
+		int cnt = 0;
+		int a = 0;
+
+		for (int i = 1; i <= N; i++) {
+			if (N % i == 0) {
+				a = i;
+				cnt++;
+			}
+			if (cnt == K) {
+				System.out.println(a);
+				break;
+			}
+		}
+		if (cnt < K) {
+			System.out.println(0);
+		}
+
+	}
+}
