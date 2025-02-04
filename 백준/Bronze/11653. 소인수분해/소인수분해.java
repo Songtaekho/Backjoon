74218361b2779e71e786d0d28a539d7cf67b2b9d
Songtaekho <thdxor17@gmail.com>
2025-02-04 14:45:51 +0900

    [Bronze I] Title: 소인수분해, Time: 204 ms, Memory: 17744 KB -BaekjoonHub

---
+++ 백준/Bronze/11653. 소인수분해/소인수분해.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		Scanner scan = new Scanner(System.in);
+		
+		int N = scan.nextInt();
+		int a = 2;
+		
+		if(N == 1) {
+			return;
+		}
+		
+		while(true) {
+			
+			if(N % a == 0) {
+				N = N /a;
+				System.out.println(a);
+			} else {
+				a++;
+			}
+			
+			if(N == 1) {
+				break;
+			}
+		}
+		
+	}
+}
