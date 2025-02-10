5907c7eb308f74f63c6283d7ee2b5cb7d3197ab1
Songtaekho <thdxor17@gmail.com>
2025-02-10 14:36:39 +0900

    [Bronze IV] Title: 별 찍기 - 3, Time: 240 ms, Memory: 18048 KB -BaekjoonHub

---
+++ 백준/Bronze/2440. 별 찍기 － 3/별 찍기 － 3.java
@@ -0,0 +1,19 @@
+
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+
+		Scanner scan = new Scanner(System.in);
+
+		int a = scan.nextInt();
+
+		for (int i = a; i >= 1; i--) {
+			for (int j = 1; j <= i; j++) {
+				System.out.print("*");
+			}
+			System.out.println();
+		}
+	}
+}
